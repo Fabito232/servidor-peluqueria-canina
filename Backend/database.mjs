633d153b93cf28c57d2789102c9c245476ef282f
Fabito232 <fabibbsi6@gmail.com>
2024-03-28 11:39:35 -0600

    carpeta

--- Backend/database.mjs
+++ Backend/database.mjs
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(process.env.MYSQL_URL);
 // Sincronizar modelos con la base de datos
 sequelize.sync()
   .then(() => {
-    console.log('Modelos sincronizados con la base de datos.');
+    console.log('Modelos sincronizados con la base de datos');
   })
   .catch(err => {
     console.error('Error al sincronizar modelos:', err);
